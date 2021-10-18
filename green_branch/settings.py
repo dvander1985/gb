f5f718375b57fd05f97ce2fc6b97588635a6ed94
denis_k <dvander@inbox.ru>
2021-10-18 06:21:12 +0400

    Ready for deployment to heroku.

--- green_branch/settings.py
+++ green_branch/settings.py
@@ -155,8 +155,10 @@ if os.getcwd() == '/app':
 	ALLOWED_HOSTS = ['*']
 
 	# Конфигурация статических ресурсов
-	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = 'staticfiles'
+	#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+	STATIC_URL = '/static/'
 	STATICFILES_DIRS = (
 		os.path.join(BASE_DIR, 'static'),
 	)
