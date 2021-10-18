3ac352bf8cfa54de51b1e1dd6049dc9b069ad200
denis_k <dvander@inbox.ru>
2021-10-18 07:14:18 +0400

    Ready for deployment to heroku.

--- green_branch/settings.py
+++ green_branch/settings.py
@@ -13,8 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 db_path = BASE_DIR / 'db.sqlite3'
 
 # Quick-start development settings - unsuitable for production
@@ -154,8 +154,8 @@ ALLOWED_HOSTS = ['*']
 # Конфигурация статических ресурсов
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
